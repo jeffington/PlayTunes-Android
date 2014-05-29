ef9044acdd3313f05a04d2add78d6510e5bd0089
Jeff Meyerhoff <jeff@jdmlabs.org>
2014-05-28 20:25:26 -0500

    Stand in settings activity

---
+++ src/com/ideabag/playtunes/activity/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.ideabag.playtunes.activity;
+
+import com.ideabag.playtunes.R;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class SettingsActivity extends ActionBarActivity {
+	
+	@Override public void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		
+		setContentView( R.layout.activity_settings );
+		
+	}
+	
+}
