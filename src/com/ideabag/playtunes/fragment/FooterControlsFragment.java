1f0d36061d4bb47b0be0107a0a0f23f7197ed69f
Jeff Meyerhoff <jeff@jdmlabs.org>
2014-07-09 11:21:54 -0500

    FooterControlsFragment and layout resource. When playing music, it slides up to show the currently playing song, a thumbnail of the album art, and a play/pause button.

--- src/com/ideabag/playtunes/fragment/FooterControlsFragment.java
+++ src/com/ideabag/playtunes/fragment/FooterControlsFragment.java
@@ -1,27 +1,38 @@
 package com.ideabag.playtunes.fragment;
 
-import com.ideabag.playtunes.MusicPlayerService.SongInfoChangedListener;
 import com.ideabag.playtunes.R;
 import com.ideabag.playtunes.activity.MainActivity;
 import com.ideabag.playtunes.activity.NowPlayingActivity;
-import com.ideabag.playtunes.adapter.ArtistAllSongsAdapter;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentManager;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class FooterControlsFragment extends Fragment {
 	
 	private MainActivity mActivity;
+	
+	private String lastAlbumUri;
+	private String current_media_id;
+	
+	private boolean isPlaying = false;
+	
+	private boolean isShowing = false;
 	
 	@Override public void onAttach( Activity activity ) {
 		
@@ -40,7 +51,7 @@ public class FooterControlsFragment extends Fragment {
     	
 		getView().findViewById( R.id.FooterControlsPlayPauseButton ).setOnClickListener( controlsClickListener );
 		
-		//mActivity.BoundService.setOnSongInfoChangedListener( this );
+		//mActivity.BoundService.setOnSongInfoChangedListener( MusicStateChanged );
 		
 	}
 	
@@ -60,9 +71,9 @@ public class FooterControlsFragment extends Fragment {
 		super.onPause();
 		
 		
-		
 	}
 	
+
 	
 	
 	View.OnClickListener controlsClickListener = new View.OnClickListener() {
@@ -74,16 +85,23 @@ public class FooterControlsFragment extends Fragment {
 			
 			if ( id == R.id.FooterControls ) {
 				
-				Intent i = new Intent( mActivity, NowPlayingActivity.class );
+				Intent startNowPlayingActivity = new Intent( getActivity(), NowPlayingActivity.class );
 				
-				mActivity.startActivityForResult( i, 0 );//( i );
+				//startNowPlayingActivity.putExtra( "title", mActivity.actionbarTitle );
+				
+				getActivity().startActivityForResult( startNowPlayingActivity, 0 );//( i );
 				
 			} else if ( id == R.id.FooterControlsPlayPauseButton ) {
 				
-				mActivity.BoundService.pause();
-				
-				// or Play
-				//mActivity.BoundService.play();
+				if ( isPlaying ) {
+					
+					mActivity.mBoundService.pause();
+					
+				} else {
+					
+					mActivity.mBoundService.play();
+					
+				}
 				
 			}
 			
@@ -91,6 +109,146 @@ public class FooterControlsFragment extends Fragment {
 		}
 		
 	};
-
+	
+	   public void setMediaID( String media_id ) {
+	    	
+		   android.util.Log.i("MediaID", ( null == media_id ? "Is Null" : media_id ) );
+		   
+	    	if ( null == media_id) {
+	    		
+	    		if ( isShowing ) {
+		    		
+	    			// Hide the fragment
+		    		//AnimationSet mSlideDown = ( AnimationSet ) AnimationUtils.loadAnimation( getActivity(), R.anim.slide_down );
+		    		
+		    		//mSlideDown.setFillAfter( true );
+		    		
+		    		//getView().setAnimation(  );
+		    		//getView().startAnimation( mSlideDown );
+		    		isShowing = false;
+	    			FragmentManager fm = getActivity().getSupportFragmentManager();
+	    			fm.beginTransaction()
+	    			          .setCustomAnimations( R.anim.slide_up, R.anim.slide_down )
+	    			          .hide( this )
+	    			          .commit();
+	    			
+	    		} else {
+	    			
+	    			FragmentManager fm = getActivity().getSupportFragmentManager();
+	    			fm.beginTransaction()
+	    			          //.setCustomAnimations( R.anim.slide_up, R.anim.slide_down )
+	    			          .hide( this )
+	    			          .commit();
+	    			
+	    			//getView().setVisibility( View.GONE );
+	    			
+	    		}
+	    		
+	    	} else if ( media_id != this.current_media_id ) {
+	    		
+	    		if ( !isShowing ) {
+	    			
+	    			FragmentManager fm = getActivity().getSupportFragmentManager();
+	    			fm.beginTransaction()
+	    			          .setCustomAnimations( R.anim.slide_up, R.anim.slide_down )
+	    			          .show( this )
+	    			          .commit();
+		    		
+		    		isShowing = true;
+	    			
+	    		}
+	    		
+	    		this.current_media_id = media_id;
+	    		
+	    		Cursor mSongCursor = mActivity.getContentResolver().query(
+						MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+						new String[] {
+							
+							MediaStore.Audio.Media.ALBUM,
+							MediaStore.Audio.Media.ARTIST,
+							MediaStore.Audio.Media.TITLE,
+							MediaStore.Audio.Media.ALBUM_ID,
+							MediaStore.Audio.Media.DURATION,
+							MediaStore.Audio.Media._ID
+							
+						},
+						MediaStore.Audio.Media._ID + "=?",
+						new String[] {
+							
+								media_id
+							
+						},
+						null
+					);
+				
+	    		android.util.Log.i( "FooterControlsFragment", "Current song cursor : " + mSongCursor.getCount() );
+	    		
+				mSongCursor.moveToFirst();
+				
+				String title = mSongCursor.getString( mSongCursor.getColumnIndexOrThrow( MediaStore.Audio.Media.TITLE ) );
+				String artist = mSongCursor.getString( mSongCursor.getColumnIndexOrThrow( MediaStore.Audio.Media.ARTIST ) );
+				
+				String album_id = mSongCursor.getString( mSongCursor.getColumnIndexOrThrow( MediaStore.Audio.Media.ALBUM_ID ) );
+				
+				Cursor albumCursor = mActivity.getContentResolver().query(
+						MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
+					    new String[] {
+					    	
+					    	MediaStore.Audio.Albums.ALBUM_ART,
+					    	MediaStore.Audio.Albums._ID
+					    	
+					    },
+					    MediaStore.Audio.Albums._ID + "=?",
+						new String[] {
+							
+							album_id
+							
+						},
+						null
+					);
+				
+				albumCursor.moveToFirst();
+				
+				String newAlbumUri = albumCursor.getString( albumCursor.getColumnIndexOrThrow( MediaStore.Audio.Albums.ALBUM_ART ) );
+				
+				if ( !newAlbumUri.equals( lastAlbumUri ) ) {
+					
+					lastAlbumUri = newAlbumUri;
+					
+					Uri albumArtUri = Uri.parse( newAlbumUri );
+					
+					( ( ImageView ) getView().findViewById( R.id.FooterControlsAlbumArt ) ).setImageURI( albumArtUri );
+				}
+				
+				albumCursor.close();
+				
+				( ( TextView ) getView().findViewById( R.id.FooterControlsSongTitle ) ).setText( title );
+				( ( TextView ) getView().findViewById( R.id.FooterControlsArtistName ) ).setText( artist );
+				
+				
+	    	}
+	    	
+	    }
+	   
+	   
+	   public void showPaused() {
+		   
+		   this.isPlaying = false;
+		   
+		   ( ( ImageButton ) getView().findViewById( R.id.FooterControlsPlayPauseButton ) ).setImageResource( R.drawable.ic_action_playback_play_white );
+		   
+		   ( ( TextView ) getView().findViewById( R.id.FooterControlsSongTitle ) ).setEllipsize( null );
+		   
+	   }
+	   
+	   public void showPlaying() {
+		   
+		   this.isPlaying = true;
+		   
+		   ( ( ImageButton ) getView().findViewById( R.id.FooterControlsPlayPauseButton ) ).setImageResource( R.drawable.ic_action_playback_pause_white );
+		   
+		   ( ( TextView ) getView().findViewById( R.id.FooterControlsSongTitle ) ).setEllipsize( TextUtils.TruncateAt.MARQUEE );
+		   
+	   }
 	
 }
