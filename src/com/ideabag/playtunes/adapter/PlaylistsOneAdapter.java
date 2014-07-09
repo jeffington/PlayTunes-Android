6feea50bf56e7a7bd6a731aebaa3143ef3134dac
Jeff Meyerhoff <jeff@jdmlabs.org>
2014-07-09 11:56:50 -0500

    Put the media/song id as the first column in the cursor query, now handles favorite button and song menu button.

--- src/com/ideabag/playtunes/adapter/PlaylistsOneAdapter.java
+++ src/com/ideabag/playtunes/adapter/PlaylistsOneAdapter.java
@@ -1,5 +1,6 @@
 package com.ideabag.playtunes.adapter;
 
+import com.ideabag.playtunes.PlaylistManager;
 import com.ideabag.playtunes.R;
 
 import android.content.Context;
@@ -10,50 +11,75 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class PlaylistsOneAdapter extends BaseAdapter {
 	
 	protected Context mContext;
 	protected Cursor cursor;
 	
-	private static final char MUSIC_NOTE = (char) 9834;
+	private PlaylistManager PlaylistManager;
+	private View.OnClickListener songMenuClickListener;
 	
 	private String PLAYLIST_ID;
 	public String PLAYLIST_NAME;
 	
-    private static final String[] singlePlaylistSelection = new String[] { // And this...
-    	/*
-		MediaStore.Audio.Media.TITLE,
-		MediaStore.Audio.Media.ARTIST,
-		MediaStore.Audio.Media.TRACK,
-		MediaStore.Audio.Media.ALBUM,
-		MediaStore.Audio.Media.DATA,
-		MediaStore.Audio.Media.ALBUM_ID,
-		*/
+	private boolean isEditing = false;
+	
+    private static final String[] singlePlaylistSelection = new String[] {
+    	
+    	// The ID of the media item must be the first thing in the selection
+    	MediaStore.Audio.Playlists.Members.AUDIO_ID,
+    	
     	MediaStore.Audio.Playlists.Members.TITLE,
     	MediaStore.Audio.Playlists.Members.ARTIST,
     	MediaStore.Audio.Playlists.Members.ALBUM,
-		//MediaStore.Audio.Playlists._ID,
+    	MediaStore.Audio.Playlists.Members.DATA,
+    	MediaStore.Audio.Playlists.Members._ID,
+    	MediaStore.Audio.Playlists.Members.PLAYLIST_ID
 		
 	};
     
-	public PlaylistsOneAdapter( Context context, String playlist_id ) {
+	public PlaylistsOneAdapter( Context context, String playlist_id, View.OnClickListener menuClickListener ) {
 		
 		mContext = context;
 		PLAYLIST_ID = playlist_id;
-		//PLAYLIST_NAME = name;
 		
-		cursor = context.getContentResolver().query(
+		PlaylistManager = new PlaylistManager( mContext );
+		
+		songMenuClickListener = menuClickListener;
+		
+		//android.util.Log.i( "starred adapter", "" + playlist_id );
+		
+		requery();
+		
+	}
+	
+	public Cursor getCursor() {
+		
+		return cursor;
+		
+	}
+	
+	public void requery() {
+		
+		if ( null != cursor ) {
+			
+			cursor.close();
+			
+		}
+		
+		cursor = mContext.getContentResolver().query(
 				MediaStore.Audio.Playlists.Members.getContentUri( "external", Long.parseLong( PLAYLIST_ID ) ),
 				singlePlaylistSelection,
-				null,
-				null,
+				MediaStore.Audio.Playlists.Members.PLAYLIST_ID + "=?",
+				new String[] {
+					
+					PLAYLIST_ID
+					
+				},
 				null
 			);
-	
-		//cursor.moveToFirst();
-		
-		
 		
 	}
 	
@@ -83,6 +109,9 @@ public class PlaylistsOneAdapter extends BaseAdapter {
 			
 			convertView = li.inflate( R.layout.list_item_song_no_album, null );
 			
+			convertView.findViewById( R.id.StarButton ).setOnClickListener( songMenuClickListener );
+			convertView.findViewById( R.id.MenuButton ).setOnClickListener( songMenuClickListener );
+			
 		}
 		
 		cursor.moveToPosition( position );
@@ -90,12 +119,41 @@ public class PlaylistsOneAdapter extends BaseAdapter {
 		String songTitle = cursor.getString( cursor.getColumnIndexOrThrow( MediaStore.Audio.Playlists.Members.TITLE ) );
 		String songArtist = cursor.getString( cursor.getColumnIndexOrThrow( MediaStore.Audio.Media.ARTIST ) );
 		String songAlbum = cursor.getString( cursor.getColumnIndexOrThrow( MediaStore.Audio.Media.ALBUM ) );
+		String song_id = cursor.getString( cursor.getColumnIndexOrThrow( MediaStore.Audio.Playlists.Members.AUDIO_ID ) );
 		
 		( ( TextView ) convertView.findViewById( R.id.SongTitle )).setText( songTitle );
 		( ( TextView ) convertView.findViewById( R.id.SongArtist )).setText( songArtist );
 		( ( TextView ) convertView.findViewById( R.id.SongAlbum )).setText( songAlbum );
 		
+		convertView.findViewById( R.id.StarButton ).setTag( R.id.tag_song_id, song_id );
+		
+		ToggleButton starButton = ( ToggleButton ) convertView.findViewById( R.id.StarButton );
+		
+		starButton.setChecked( PlaylistManager.isStarred( song_id ) ); 
+		
+		if ( !this.isEditing ) {
+			
+			convertView.findViewById( R.id.StarButton ).setVisibility( View.VISIBLE );
+			convertView.findViewById( R.id.MenuButton ).setVisibility( View.VISIBLE );
+			convertView.findViewById( R.id.DragButton ).setVisibility( View.GONE );
+			
+		} else {
+			
+			convertView.findViewById( R.id.StarButton ).setVisibility( View.GONE );
+			convertView.findViewById( R.id.MenuButton ).setVisibility( View.GONE );
+			convertView.findViewById( R.id.DragButton ).setVisibility( View.VISIBLE );
+			
+		}
+		
 		return convertView;
+		
+	}
+	
+	
+	public void setEditing( boolean editing ) {
+		
+		this.isEditing = editing;
+		this.notifyDataSetChanged();
 		
 	}
 
