d4d1453983863244d4512c32c18725cdce662352
Jeff Meyerhoff <jeff@jdmlabs.org>
2014-08-04 15:02:52 -0500

    Removed unused references.

--- src/com/ideabag/playtunes/dialog/RateAppDialogFragment.java
+++ src/com/ideabag/playtunes/dialog/RateAppDialogFragment.java
@@ -1,10 +1,7 @@
 package com.ideabag.playtunes.dialog;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
