f1595d091d4438ff345f80fc5a9cb96c60dc34b9
Jeff Meyerhoff <jeff@jdmlabs.org>
2014-09-25 00:39:22 -0500

    Added tracking, fixed setting the now playing fragment.

--- src/com/ideabag/playtunes/fragment/search/SearchAlbumsFragment.java
+++ src/com/ideabag/playtunes/fragment/search/SearchAlbumsFragment.java
@@ -1,5 +1,6 @@
 package com.ideabag.playtunes.fragment.search;
 
+import android.app.Activity;
 import android.database.ContentObserver;
 import android.os.Bundle;
 import android.os.Handler;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.ToggleButton;
 
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
 import com.ideabag.playtunes.R;
 import com.ideabag.playtunes.activity.MainActivity;
 import com.ideabag.playtunes.adapter.search.SearchAlbumsAdapter;
@@ -16,14 +19,26 @@ import com.ideabag.playtunes.dialog.SongMenuDialogFragment;
 import com.ideabag.playtunes.fragment.SaveScrollListFragment;
 import com.ideabag.playtunes.fragment.SongsFragment;
 import com.ideabag.playtunes.util.ISearchable;
+import com.ideabag.playtunes.util.TrackerSingleton;
+import com.ideabag.playtunes.util.GAEvent.Categories;
+import com.ideabag.playtunes.util.GAEvent.Playlist;
 
 public class SearchAlbumsFragment extends SaveScrollListFragment implements ISearchable {
+	public static final String TAG = "Album Search Fragment";
 	
 	private static final int SEARCH_RESULT_NO_LIMIT = -1;
 	
 	private MainActivity mActivity;
+	private Tracker mTracker;
 	private SearchAlbumsAdapter adapter;
 	String mQuery;
+	
+	@Override public void onAttach( Activity activity ) {
+		super.onAttach( activity );
+		
+		mActivity = ( MainActivity ) activity;
+		mTracker = TrackerSingleton.getDefaultTracker( mActivity );
+	}
 	
 	@Override public void onActivityCreated( Bundle savedInstanceState ) {
 		super.onActivityCreated( savedInstanceState );
@@ -37,6 +52,21 @@ public class SearchAlbumsFragment extends SaveScrollListFragment implements ISea
 		adapter = new SearchAlbumsAdapter( getActivity(), mQuery, SEARCH_RESULT_NO_LIMIT );
 		
 		setListAdapter( adapter );
+		
+	}
+	
+	@Override public void onResume() {
+		super.onResume();
+		
+    	mTracker.setScreenName( TAG );
+    	mTracker.send( new HitBuilders.AppViewBuilder().build() );
+		
+    	
+    	mTracker.send( new HitBuilders.EventBuilder()
+    	.setCategory( Categories.PLAYLIST )
+    	.setAction( Playlist.ACTION_SHOWLIST )
+    	.setValue( adapter.getCount() )
+    	.build());
 		
 	}
 	
@@ -57,52 +87,20 @@ public class SearchAlbumsFragment extends SaveScrollListFragment implements ISea
 		
 		String playlistName = mActivity.getSupportActionBar().getTitle().toString();
 		
-		mActivity.mBoundService.setPlaylist( adapter.getQuery(), playlistName, SongsFragment.class, null );
+		mActivity.mBoundService.setPlaylist( adapter.getQuery(), playlistName, SearchFragment.class, mQuery );
 		//mActivity.mBoundService.setPlaylistCursor( adapter.getCursor() );
 		
 		mActivity.mBoundService.setPlaylistPosition( position );
 		
 		mActivity.mBoundService.play();
 		
+		mTracker.send( new HitBuilders.EventBuilder()
+    	.setCategory( Categories.PLAYLIST )
+    	.setAction( Playlist.ACTION_CLICK )
+    	.setValue( position )
+    	.build());
+		
 	}
-
-	View.OnClickListener songMenuClickListener = new View.OnClickListener() {
-		
-		@Override public void onClick( View v ) {
-			
-			int viewID = v.getId();
-			String songID = "" + v.getTag( R.id.tag_song_id );
-			
-			if ( viewID == R.id.StarButton ) {
-				
-				ToggleButton starButton = ( ToggleButton ) v;
-				
-				if ( starButton.isChecked() ) {
-					
-					mActivity.PlaylistManager.addFavorite( songID );
-					//android.util.Log.i( "starred", songID );
-					
-				} else {
-					
-					mActivity.PlaylistManager.removeFavorite( songID );
-					//android.util.Log.i( "unstarred", songID );
-					
-				}
-				
-			} else if ( viewID == R.id.MenuButton ) {
-				
-				FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
-	        	
-				SongMenuDialogFragment newFragment = new SongMenuDialogFragment();
-				newFragment.setMediaID( songID );
-	        	
-	            newFragment.show( ft, "dialog" );
-				
-			}
-			
-		}
-		
-	};
 	
 	ContentObserver mediaStoreChanged = new ContentObserver(new Handler()) {
 
