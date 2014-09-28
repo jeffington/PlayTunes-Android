cfbb9a98efbc394199465635a3571d3efd10ce38
Jeff Meyerhoff <jeff@jdmlabs.org>
2014-09-28 10:28:07 -0500

    Adds async callback.

--- src/com/ideabag/playtunes/adapter/AlbumsAllAdapter.java
+++ src/com/ideabag/playtunes/adapter/AlbumsAllAdapter.java
@@ -4,15 +4,7 @@ import com.ideabag.playtunes.R;
 import com.ideabag.playtunes.database.MediaQuery;
 
 import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
 import android.provider.MediaStore;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class AlbumsAllAdapter extends AlbumListAdapter {
 	
@@ -27,7 +19,7 @@ public class AlbumsAllAdapter extends AlbumListAdapter {
     	
     };
 	
-	public AlbumsAllAdapter( Context context ) {
+	public AlbumsAllAdapter( Context context, MediaQuery.OnQueryCompletedListener listener ) {
 		super( context );
 		
 		mQuery = new MediaQuery(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
@@ -38,6 +30,8 @@ public class AlbumsAllAdapter extends AlbumListAdapter {
 				},
 				MediaStore.Audio.Albums.DEFAULT_SORT_ORDER
 			);
+		
+		setOnQueryCompletedListener( listener );
 		
 		requery();
 		
