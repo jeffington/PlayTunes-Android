acf180cd29732fc4b73e09fe9ceea9fbaf917ced
Jeff Meyerhoff <jeff@jdmlabs.org>
2014-09-28 10:20:45 -0500

    Created a generic adapter to execute Async MediaQuerys.

---
+++ src/com/ideabag/playtunes/adapter/AsyncQueryAdapter.java
@@ -0,0 +1,96 @@
+package com.ideabag.playtunes.adapter;
+
+import com.ideabag.playtunes.database.MediaQuery;
+import com.ideabag.playtunes.database.MediaQuery.OnQueryCompletedListener;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class AsyncQueryAdapter extends BaseAdapter implements MediaQuery.OnQueryCompletedListener {
+	
+	protected Context mContext;
+	
+	protected Cursor mCursor = null;
+	protected MediaQuery mQuery = null;
+	protected MediaQuery.OnQueryCompletedListener mListener = null;
+	
+	public AsyncQueryAdapter( Context context ) {
+		
+		mContext = context;
+		
+	}
+	
+    public MediaQuery getQuery() {
+    	
+    	return mQuery;
+    	
+    }
+    
+    protected void setOnQueryCompletedListener( OnQueryCompletedListener listener ) {
+    	
+    	mListener = listener;
+    	
+    }
+    
+    protected void setMediaQuery( MediaQuery query ) {
+    	
+    	mQuery = query;
+    	
+    	requery();
+    	
+    }
+    
+    //
+    // Probably depreciated, use requeryAsync instead
+	//
+    public void requery() {
+		
+    	MediaQuery.executeAsync( mContext, mQuery, this );
+		
+	}
+    
+    @Override public void onQueryCompleted( MediaQuery mQuery, Cursor mResult ) {
+		
+		if ( null != mCursor && !mCursor.isClosed() ) {
+			
+			mCursor.close();
+			
+		}
+		
+		mCursor = mResult;
+		notifyDataSetChanged();
+		
+		if ( null != mListener ) {
+			
+			mListener.onQueryCompleted( mQuery, mResult );
+			
+		}
+		
+	}
+	
+	
+	@Override public int getCount() {
+		
+		return ( null != mCursor && !mCursor.isClosed() ? mCursor.getCount() : 0 );
+		
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return 0;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		return null;
+	}
+
+}
